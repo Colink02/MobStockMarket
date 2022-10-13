187c758247cb4a257ef334a09f000372b36dceba
Colink02 <colinkinzel@gmail.com>
2022-10-12 21:11:01 -0500

    Basic setup of Plugin

---
+++ src/main/java/com/colink02dev/mobstockmarket/MobStockMarket.java
@@ -0,0 +1,17 @@
+package com.colink02dev.mobstockmarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobStockMarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
